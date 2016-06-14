bb8927242346be760f0ffb83b442f369112d906c
awesomesauce12 <abhinav.ayalur@gmail.com>
2016-06-14 14:13:01 -0700

    need to work on logic for pseudo-cluster

--- robot_recog_demo.py
+++ robot_recog_demo.py
@@ -25,11 +25,25 @@ global mx
 global my
 global gx
 global gy
+global maxVal
+global avgX
+global avgY
+global p
+global posX
+global posY
+global b
+maxVal = 0
 mx = 0
 my = 0
 gx = 0
 gy = 0 
 samples = 0
+b = 0
+avgX=[]
+avgY=[]
+p =[]
+posX= []
+posY = []
 imageFrame = np.zeros((456, 507, 3), np.uint8)
 cap = cv2.VideoCapture(0)
 
@@ -73,6 +87,33 @@ def showImage(capImg):
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
+def sortArray(x, y):
+    global avgX
+    global avgY
+    global p
+    length = len(avgX)
+    for a in range(0, length):
+        if (abs(x - avgX[a]) < 35) and (abs(y - avgY[a]) < 35):
+            #print ('calculating average')
+            avgX[a] = (x+avgX[a]*p[a])/(p[a]+1)
+            avgY[a] = (y+avgX[a]*p[a])/(p[a]+1)
+            p[a] = p[a] +1
+            return
+    avgX.append(x)
+    avgY.append(y)
+    p.append(1)
+    #print ('new average created')
+
+def checkForAverage(p):
+    global maxVal
+    for a in range(0, len(p)):
+        val = p[a]
+        if (val > maxVal):
+            maxVal = val
+            p[b] = p[a]
+    return b
+            
+
 def colorAndCornerRecognition(capImg):
     # Get image, make it hsv, filter color of interest and de-noise
     hsv = cv2.cvtColor(capImg, cv2.COLOR_BGR2HSV)
@@ -98,11 +139,31 @@ def colorAndCornerRecognition(capImg):
     ret, thresh = cv2.threshold(img, 15, 250, cv2.THRESH_BINARY)
     image, contours, heirarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
+    global avgX
+    global avgY
+    global p
+    global posX
+    global posY
     # Find the appropriate contour
-    while (existMoments == 0.0):
-        #print('Num of Contours ', len(contours))
-        avgContours = int(len(contours)/2)
-        for i in range (0, avgContours):
+    for i in range (0, len(contours)):
+        #print ( 'contour len', len(contours), 'i ', i, 'Cluster len ', len(avgX))
+        cnt = contours[i]
+        M = cv2.moments(cnt)
+        existMoments = float(M['m10'])
+        if (existMoments != 0.0):
+            x = int(M['m10']/M['m00'])
+            y = int(M['m01']/M['m00'])
+            posX.append(x)
+            posY.append(y)
+            sortArray(x, y)                
+    b = checkForAverage(p)
+    cx = avgX[b]
+    cy = avgY[b]
+            
+            
+    print('Num of Contours ', len(contours))
+        #avgContours = int(len(contours)/2)
+    '''for i in range (0, avgContours):
             #print ("i is ", i, "avg is ", avgContours )
             cnt = contours[avgContours + i]
             M = cv2.moments(cnt)
@@ -118,27 +179,27 @@ def colorAndCornerRecognition(capImg):
             iterations = 0
             iterations = iterations +1
             if (iterations > avgContours + 1):
-                break
-        global mx
-        global my
-        global samples
-        if (abs(cx - mx) < 35) and (abs(cy-my) < 35):
-            samples = samples + 1
-            #print ('Within threshold cx is', cx, ' cy is ', cy, ' mx', mx, ' my', my,
-            #       ' gx', gx, ' gy', gy, ' samples', samples)
-            cx = mx
-            cy = my
-            
+                break'''
+    global mx
+    global my
+    global samples
+    if (abs(cx - mx) < 35) and (abs(cy-my) < 35):
+        samples = samples + 1
+        #print ('Within threshold cx is', cx, ' cy is ', cy, ' mx', mx, ' my', my,
+        #       ' gx', gx, ' gy', gy, ' samples', samples)
+        cx = mx
+        cy = my
+        
+    else:
+        samples = 0
+        #print ('Moved cx is', cx, ' cy is ', cy, ' mx', mx, ' my', my,
+        #      ' gx', gx, ' gy', gy, ' samples', samples)
+        if (((abs(gx - cx)<35 and (abs(gy-cy) <35)))):
+            mx = gx
+            my = gy
         else:
-            samples = 0
-            #print ('Moved cx is', cx, ' cy is ', cy, ' mx', mx, ' my', my,
-            #      ' gx', gx, ' gy', gy, ' samples', samples)
-            if (((abs(gx - cx)<35 and (abs(gy-cy) <35)))):
-                mx = gx
-                my = gy
-            else:
-                mx = cx
-                my = cy
+            mx = cx
+            my = cy
             
             
             
@@ -153,6 +214,8 @@ def colorAndCornerRecognition(capImg):
     #global imageFrame
     #imageFrame = cv2.rectangle(imageFrame, (cx -1, cy-1), (cx+1, cy+1), (255,0,0), -1, 8, 0)
     
+    print ( 'contour len', len(contours), 'i ', i, 'Cluster len ', len(avgX))
+    print ('cx ', cx, 'cy ' , cy)
     return (cx, cy)
     #cv2.imshow(imageFrame, 'frame')
     #cv2.waitkey(0)
@@ -171,11 +234,25 @@ def perform_robot_dance(client):
     global mx
     global my
     global samples
+    global maxVal
+    global avgX
+    global avgY
+    global p
+    global posX
+    global posY
+    global b
     #limit = input("enter amount of times you want to capture: ")
     #limit = int(limit)
     start = timeit.default_timer()
     stop = start
-    while (stop - start < 30):
+    while (stop - start < 15):
+        b = 0
+        avgX=[]
+        avgY=[]
+        p =[]
+        posX= []
+        posY = []
+        
         #while (1):
         capImg = captureAndLoadImage()
         (cx, cy) = colorAndCornerRecognition(capImg)
@@ -188,13 +265,13 @@ def perform_robot_dance(client):
             imageFrame = cv2.rectangle(imageFrame, (cx -1, cy-1), (cx+1, cy+1), (255,0,0), -1, 8, 0)  
             msg_to_robot = '[1000][3][' + robotX + '][' + robotY + '][' + robotZ + ']'
             print (msg_to_robot)
-            client.sock.send(msg_to_robot.encode())
+            #client.sock.send(msg_to_robot.encode())
             #data = client.sock.recv(1024).decode()
             #print (data)
             #return (cx, cy)
             cv2.imshow('frame', imageFrame)
-            #print ('Printing dot cx is', cx, ' cy is ', cy, ' mx', mx, ' my', my,
-            #       ' gx', gx, ' gy', gy, ' samples', samples)
+            print ('Printing dot cx is', cx, ' cy is ', cy, ' mx', mx, ' my', my,
+                   ' gx', gx, ' gy', gy, ' samples', samples)
 
             gx = mx
             gy = my
@@ -206,11 +283,13 @@ def perform_robot_dance(client):
                     samples = 0
         stop = timeit.default_timer()
         #print ('time take is: ' , stop - start)
+        #print ('Printing dot cx is', cx, ' cy is ', cy, ' mx', mx, ' my', my,
+                 #  ' gx', gx, ' gy', gy, ' samples', samples)
     print ('Done')
     cv2.waitKey(0)
     
                  
 # Real code
-startClientServerThreads()
-#perform_robot_dance(client)
+#startClientServerThreads()
+perform_robot_dance(client)
 
